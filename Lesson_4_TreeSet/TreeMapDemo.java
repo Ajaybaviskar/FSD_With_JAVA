a8554bf522cbf9a58fc7a9aad4dcf7ead8615e7c
ajay <ajaybaviskar830@gmail.com>
2023-12-27 15:33:59 +0530

    Add TreeSetDemo in Lesson_4

---
+++ Lesson_4_TreeSet/TreeMapDemo.java
@@ -0,0 +1,13 @@
+import java.util.TreeMap;
+
+public class TreeMapDemo {
+    public static void main(String args[]) {
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
+        treeMap.put("B", 2);
+        treeMap.put("C", 1);
+        treeMap.put("A", 3);
+
+        System.out.println(treeMap);
+        // for intelije
+    }
+}
